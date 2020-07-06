ab3a1a403f41b8993f734ce37b3d81c6b66475e5
Marshall Lochbaum <mwlochbaum@gmail.com>
2020-07-06 13:12:56 -0400

    Formatting

--- test/dt.js
+++ test/dt.js
@@ -8,15 +8,20 @@ const t=load('cases.bqn').split('\n').filter(x=>x).map(x=>x.split(' % '))
     , test=t.map(e=>'"'+e[1]+'"').join('\n')
     , expt=t.map(e=>+e[0])
 
-var compiler = execFile(__dirname+'/../spec/dzref',[__dirname+'/../dc.bqn','{•←Compile𝕩}¨⟨'+test+'⟩'], function (error, stdout, stderr) {
-  const rslt=stdout.split('\n').filter(a=>a.length)
-            .map(a=>runWasm(a.split("‿").map(n=>+n)))
-      , pass=rslt.map((r,i)=>r===expt[i])
-      , fail=pass.map((p,i)=>p?-1:i).filter(i=>i>=0)
-  console.log(
-      fail.length
-      ? fail.map(i=>'"'+t[i][1]+'": expected '+expt[i]+' but received '+rslt[i])
-      : "All passed!"
-  )
-  process.exit(+(fail.length>0))
-});
+var compiler = execFile(
+  __dirname+'/../spec/dzref',
+  [ __dirname+'/../dc.bqn',
+    '{•←Compile𝕩}¨⟨'+test+'⟩' ],
+  function (error, stdout, stderr) {
+    const rslt=stdout.split('\n').filter(a=>a.length)
+              .map(a=>runWasm(a.split("‿").map(n=>+n)))
+        , pass=rslt.map((r,i)=>r===expt[i])
+        , fail=pass.map((p,i)=>p?-1:i).filter(i=>i>=0)
+    console.log(
+        fail.length
+        ? fail.map(i=>'"'+t[i][1]+'": expected '+expt[i]+' but received '+rslt[i])
+        : "All passed!"
+    )
+    process.exit(+(fail.length>0))
+  }
+);
